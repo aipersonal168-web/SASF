857ca3b357e197f6bfb67bb05e0b36c9007bb04c
EM METH <methlovestudentsandwork@gmail.com>
2025-12-28 21:28:19 +0700

    Update DashbordController.php

--- app/Http/Controllers/Dashboard/DashbordController.php
+++ app/Http/Controllers/Dashboard/DashbordController.php
@@ -14,7 +14,7 @@ class DashbordController extends Controller
  public function index()
 {
     // Call Node.js API
-    $response = Http::get('http://localhost:5000/api/students/count');
+    $response = Http::get('https://sas-ecrt.onrender.com/api/students/count');
 
     // Convert response to array
     $data = $response->json();
